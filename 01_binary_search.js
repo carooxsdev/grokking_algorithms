75c7e5b06a48a5cc1192f07a9788a0bdfcdad006
carooxsdev <carooxsdev@gmail.com>
2021-11-22 15:42:01 -0300

    Exercício 01 - Pesquisa binária

---
+++ 01_binary_search.js
@@ -0,0 +1,24 @@
+function pesquisa_binária(lista, item) {
+  let baixo = 0
+  let alto = lista.length - 1
+
+  while (baixo <= alto) {
+    let meio = Math.floor((baixo + alto) / 2)
+    let chute = lista[meio]
+
+    if (chute === item) {
+      return meio
+    }
+    if (chute > item) {
+      alto = meio - 1
+    } else {
+      baixo = meio + 1
+    }
+  }
+
+  return null
+}
+
+const minha_lista = [1, 3, 5, 7, 9]
+console.log(pesquisa_binária(minha_lista, 3)) //posição 1 do array
+console.log(pesquisa_binária(minha_lista, -1)) // Não existe no array, retorna Null
